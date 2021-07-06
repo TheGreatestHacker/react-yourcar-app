846aa5e99def0c9992370e9887e8d4f721bbb284
Bryan Cruz <bryancruz457@hotmail.com>
2021-07-06 15:59:19 -0500

    Add about us component

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -6,6 +6,7 @@ import { TopSection } from './topSection';
 import { BookCard } from "../../components/bookCard";
 import { Marginer } from '../../components/marginer';
 import { BookingSteps } from './bookingSteps';
+import { AboutUs } from "./aboutUs";
 
 const PageContainer = styled.div`
   ${tw`
@@ -27,6 +28,8 @@ export function HomePage() {
       <BookCard />
       <Marginer direction="vertical" margin="8em" />
       <BookingSteps />
+      <Marginer direction="vertical" margin="8em" />
+      <AboutUs />
     </PageContainer>
   )
 }
